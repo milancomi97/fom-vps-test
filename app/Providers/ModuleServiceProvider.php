ea3361cd5baa91517cbd908cf67c59f4badd42b7
milandrsm <119659032+milandrsm@users.noreply.github.com>
2023-11-13 19:42:50 +0100

    Fix Module Provider for Linux

--- app/Providers/ModuleServiceProvider.php
+++ app/Providers/ModuleServiceProvider.php
@@ -21,26 +21,22 @@ class ModuleServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        foreach (glob(base_path('app\Modules\*')) ?: [] as $dir) {
+        foreach (glob(base_path('app/Modules/*')) ?: [] as $dir) {
             Log::channel('vps_debug')->debug('DIR:'.$dir);
 
             $modelClassName = class_basename($dir);
             $path = Str::before($dir, "\\$modelClassName");
-            $moduleRouteFile = "$path\\$modelClassName"."\\$modelClassName" . 'Routes.php';
+            $moduleRouteFile = $path."/".$modelClassName . 'Routes.php';
 
             if (!file_exists($moduleRouteFile)) continue;
 
-            Log::channel('vps_debug')->debug('moduleRouteFile: '.$moduleRouteFile);
 
             $this->loadRoutesFrom($moduleRouteFile);
 
-            Log::channel('vps_debug')->debug('Views path: '."$path\\".$modelClassName.'\views');
 
-            $this->loadViewsFrom("$path\\".$modelClassName.'\views',Str::lower($modelClassName));
+            $this->loadViewsFrom($path.'/views',Str::lower($modelClassName));
 
-            Log::channel('vps_debug')->debug('ModelClassName: '.$modelClassName);
-
-//            $this->publishes([
+//         $this->publishes([
 //                "$path\\".$modelClassName.'\views' => resource_path('views\vendor\\'.Str::lower($modelClassName)),
 //            ]);
         }
