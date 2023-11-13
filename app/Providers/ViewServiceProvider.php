ee931d13e28f3b2bc265e124bcce144005b3b54d
milandrsm <119659032+milandrsm@users.noreply.github.com>
2023-11-13 22:34:53 +0100

    Linux server fix 6

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
@@ -27,10 +28,12 @@ class ViewServiceProvider extends ServiceProvider
             $modelClassName = class_basename($dir);
             $path = Str::before($dir, "\\$modelClassName");
 
+            Log::channel('vps_debug')->debug('$path:'.$path);
 
            // LINUX            $this->loadViewsFrom($path.'/views',Str::lower($modelClassName));
 // WINDOWS            $this->loadViewsFrom("$path\\".$modelClassName.'\views',Str::lower($modelClassName));
             $this->loadViewsFrom($path.'/views',Str::lower($modelClassName));
+            Log::channel('vps_debug')->debug('$viewPath:'.$path.'/views');
 
             view()->composer('*', function ($view) {
                 $view->with('permissions',
