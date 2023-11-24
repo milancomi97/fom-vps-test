4c14caf80a59c6958fba13ef440084e17d195744
milandrsm <119659032+milandrsm@users.noreply.github.com>
2023-11-24 14:52:32 +0100

    User agent data

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -8,7 +8,13 @@ class PermissionController extends Controller
 {
 
     public function index(Request $request){
-        return "Nemate pristup modulu"; // TODO add page and back button
+
+        $ipAdresa = $request->ip();
+        $userAgent = $request->server('HTTP_USER_AGENT');
+        $platform = $request->server('HTTP_SEC_CH_UA_PLATFORM');
+
+
+        return "Nemate pristup modulu" ."<br>"."IP adressa: ".$ipAdresa."<br>"."User agent: ".$userAgent."<br>"."Platform: ".$platform; // TODO add page and back button
 
     }
 }
