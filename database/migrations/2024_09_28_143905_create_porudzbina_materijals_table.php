69dec7c202f48807d8cbbebc9e9a1b51f6cf2460
Milan Dimitrijevic <119659032+milandrsm@users.noreply.github.com>
2024-09-29 23:15:09 +0200

    porudzbina materijal

---
+++ database/migrations/2024_09_28_143905_create_porudzbina_materijals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('porudzbina_materijals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('porudzbina_id');
+            $table->unsignedBigInteger('materijal_id');
+            $table->foreign('porudzbina_id')->references('id')->on('porudzbines')->onDelete('cascade');
+            $table->foreign('materijal_id')->references('sifra_materijala')->on('materijals')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('porudzbina_materijals');
+    }
+};
