06ff6025a94bf97db0e2a6be78977726320c1501
Milan Dimitrijevic <119659032+milandrsm@users.noreply.github.com>
2024-10-14 20:42:20 +0200

    komanda pocetak

---
+++ app/Console/Commands/ExportModelToCSV.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use League\Csv\Writer;
+
+class ExportModelToCSV extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+
+    protected $signature = 'export:podaci {maticni_broj} {--podaci}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+
+    public function podaciZaIspis(){
+
+
+        $model = $this->argument('maticni_broj');
+
+        $zaglavlje=['1'];
+
+        $podaci=[[$model]];
+
+
+        return [
+            'zaglavlje'=>$zaglavlje,
+            'podaci'=>$podaci,
+        ];
+    }
+
+
+    public function handle()
+    {
+        $filepath = storage_path('csv_files/model_export.csv');
+
+        $data = $this->podaciZaIspis();
+
+
+
+        // Create CSV writer instance
+        $csv = Writer::createFromPath($filepath, 'w+');
+
+        // Get column names from the model
+        $columns = array_keys($data['zaglavlje']);
+
+        // Insert header row
+        $csv->insertOne($columns);
+
+        // Insert data rows
+        foreach ($data['podaci'] as $row) {
+            $csv->insertOne($row);
+        }
+
+        $csv->output(); // This will print CSV content directly to the console
+
+    }
+}
