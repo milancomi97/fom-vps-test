3ac9dfd70b921a0d45f1ed5548374e1cd1ade756
Milan Dimitrijevic <119659032+milandrsm@users.noreply.github.com>
2024-12-09 22:45:07 +0100

    proba stampa 250

--- app/Modules/Obracunzarada/Controllers/DatotekaobracunskihExportController.php
+++ app/Modules/Obracunzarada/Controllers/DatotekaobracunskihExportController.php
@@ -424,7 +424,7 @@ class DatotekaobracunskihExportController extends Controller
         foreach ($zarDataKeys as $zar) {
             $counter++;
 
-            if ($counter < 200) {
+            if ($counter < 250) {
                 $radnikMaticniId = $zar->maticni_broj;
                 $radnikData = $this->obradaObracunavanjeService->pripremaPodatakaRadnik($monthId, $radnikMaticniId);
                 $mdrData = $this->maticnadatotekaradnikaInterface->where('MBRD_maticni_broj', $radnikMaticniId)->get()->first();
